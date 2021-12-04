b999ee73a3a25c5c489c06ba65815ded3c4dd76f
Gerasimos Tzakis <gertzakis@gmail.com>
2021-12-04 15:38:09 +0200

    made change for both steps working

--- day_04/day_04.py
+++ day_04/day_04.py
@@ -2,11 +2,10 @@ from tools.read_file import read_file
 import sys
 
 
-
-def find_bingo_boards(data:list) -> list:
+def find_bingo_boards(data: list) -> list:
     boards = []
     board = []
-    
+
     for line in data:
         if line:
             board.append(line.split())
@@ -16,40 +15,48 @@ def find_bingo_boards(data:list) -> list:
 
     return boards
 
-def calculate_board(boards:list, numbers: str): 
-    
+
+def calculate_board(boards: list, numbers: str, first_board: bool) -> int:
+
     marked = []
     winner_board = [False for _ in range(len(boards))]
     for board in boards:
-        marked.append([  [False for col in range(5) ]  for row in range(5) ])
+        marked.append([[False for col in range(5)] for row in range(5)])
 
-    for num  in numbers.split(","):
+    for num in numbers.split(","):
         for i, board in enumerate(boards):
             for row in range(5):
                 for col in range(5):
                     if board[row][col] == num:
                         marked[i][row][col] = True
 
-            won = False
+            won_board = False
             for row in range(5):
-                ok = True
+                won = True
                 for col in range(5):
                     if not marked[i][row][col]:
-                        ok = False
-                if ok:
-                    won = True
-                    
+                        won = False
+                if won:
+                    won_board = True
+
             for col in range(5):
-                ok = True
+                won = True
                 for row in range(5):
                     if not marked[i][row][col]:
-                        ok = False
-                        
-                if ok:
-                    won = True
-                
-                
-            if won:
+                        won = False
+                if won:
+                    won_board = True
+
+            if won_board and first_board:
+                unmarked_numbers = 0
+                for row in range(5):
+                    for col in range(5):
+                        if not marked[i][row][col]:
+                            unmarked_numbers += int(board[row][col])
+
+                return unmarked_numbers * int(num)
+
+            elif won_board:
                 winner_board[i] = True
                 if all([winner_board[j] for j in range(len(boards))]):
                     unmarked_numbers = 0
@@ -57,26 +64,19 @@ def calculate_board(boards:list, numbers: str):
                         for col in range(5):
                             if not marked[i][row][col]:
                                 unmarked_numbers += int(board[row][col])
-                                
-                    print(unmarked_numbers * int(num))
-                    sys.exit(0)
-        
 
-  
-    
-
+                    return unmarked_numbers * int(num)
 
 
 def main():
     file_data = read_file("day_04/input.txt")
-    
+
     loto_numbers = file_data[0]
     boards = find_bingo_boards(file_data[1:])
-    
-    calculate_board(boards, loto_numbers)
-        
-    # for board in boards:
-    #     print(board)
-    # print(boards)
-    
+    first_board = calculate_board(boards, loto_numbers, True)
+    last_board = calculate_board(boards, loto_numbers, False)
+    print("First winning board:", first_board)
+    print("Last winning board:", last_board)
+
+
 main()

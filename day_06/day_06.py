c3da85655f1d3210b111f4ef39723cc94bd57058
gertzakis <gertzakis@gmail.com>
2021-12-06 10:22:17 +0200

    day_06, nice oneeeee. it took a while to thnk dict

---
+++ day_06/day_06.py
@@ -0,0 +1,40 @@
+from tools.read_file import read_file
+
+
+def calculate_fish(data: list, days: int) -> list:
+
+    fish_list = [int(x) for x in data]
+    fish_map = {k: 0 for k in range(9)}
+
+    for fish in fish_list:
+        fish_map[fish] += 1
+
+    for day in range(days):
+        initial_zero = fish_map[0]
+        fish_map[0] = fish_map[1]
+        fish_map[1] = fish_map[2]
+        fish_map[2] = fish_map[3]
+        fish_map[3] = fish_map[4]
+        fish_map[4] = fish_map[5]
+        fish_map[5] = fish_map[6]
+        fish_map[6] = fish_map[7] + initial_zero
+        fish_map[7] = fish_map[8]
+        fish_map[8] = initial_zero
+
+    return fish_map
+
+
+def main():
+    file_data = read_file("day_06/input.txt")
+    file_data = file_data[0].split(",")
+    # print(file_data)
+    fish_map = calculate_fish(file_data, 80)
+    print(fish_map)
+    print("Sum after 80 days:", sum(fish_map.values()))
+    fish_map = calculate_fish(file_data, 256)
+    print(fish_map)
+    print("Sum after 256 days:", sum(fish_map.values()))
+
+
+
+main()

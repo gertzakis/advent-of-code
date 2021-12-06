852eb7e20ed58626158a232e40fa9184a7b78286
gertzakis <gertzakis@gmail.com>
2021-12-06 13:35:25 +0200

    with new_map

--- day_06/day_06.py
+++ day_06/day_06.py
@@ -10,16 +10,25 @@ def calculate_fish(data: list, days: int) -> list:
         fish_map[fish] += 1
 
     for day in range(days):
-        initial_zero = fish_map[0]
-        fish_map[0] = fish_map[1]
-        fish_map[1] = fish_map[2]
-        fish_map[2] = fish_map[3]
-        fish_map[3] = fish_map[4]
-        fish_map[4] = fish_map[5]
-        fish_map[5] = fish_map[6]
-        fish_map[6] = fish_map[7] + initial_zero
-        fish_map[7] = fish_map[8]
-        fish_map[8] = initial_zero
+        new_fish_map = {k: 0 for k in range(9)}
+        for key, value in fish_map.items():
+            if key == 0:
+                new_fish_map[6] += value
+                new_fish_map[8] += value
+            else:
+                new_fish_map[key - 1] += value
+
+        fish_map = new_fish_map
+        # initial_zero = fish_map[0]
+        # fish_map[0] = fish_map[1]
+        # fish_map[1] = fish_map[2]
+        # fish_map[2] = fish_map[3]
+        # fish_map[3] = fish_map[4]
+        # fish_map[4] = fish_map[5]
+        # fish_map[5] = fish_map[6]
+        # fish_map[6] = fish_map[7] + initial_zero
+        # fish_map[7] = fish_map[8]
+        # fish_map[8] = initial_zero
 
     return fish_map
 
@@ -27,7 +36,7 @@ def calculate_fish(data: list, days: int) -> list:
 def main():
     file_data = read_file("day_06/input.txt")
     file_data = file_data[0].split(",")
-    # print(file_data)
+
     fish_map = calculate_fish(file_data, 80)
     print(fish_map)
     print("Sum after 80 days:", sum(fish_map.values()))

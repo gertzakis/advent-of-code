57e7403080db994a16a3d04a8d4bc87be7ea43e3
gertzakis <gertzakis@gmail.com>
2021-12-07 10:08:59 +0200

    day 7 but is fricking slow

--- day_07/day_07.py
+++ day_07/day_07.py
@@ -2,25 +2,38 @@ from tools.read_file import read_file
 
 
 
-def find_position(data: list):
+def find_position(data: list, expensive: bool) -> float:
     data.sort()
     position = 0
     med =data[len(data) // 2]
+    minfuel = float("inf")
 
-    for pos in data:
-        position += abs(pos - med)
-        
-    return position
+    for i in range(min(data), max(data) + 1):
+        fuel = 0
+
+        for pos in data:
+            distance = abs(pos - i)
+
+            if not expensive:
+                fuel += distance
+            else:
+                for j in range(1, distance + 1):
+                    fuel += j
+
+        if minfuel > fuel:
+            minfuel = fuel
+
+    return minfuel
 
 
 def main():
     file_data = read_file("day_07/input.txt")
     file_data = [int(x) for x in file_data[0].split(",")]
 
-    position = find_position(file_data)
-    print(position)
-    # position = find_expensive_position(file_data)
-    # print(position)
+    fuel = find_position(file_data, False)
+    print(fuel)
+    fuel = find_position(file_data, True)
+    print(fuel)
     # print("average:", sum(file_data) / len(file_data))
 
 
